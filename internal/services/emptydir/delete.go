b26c1a8b7ac124929642dd0859ed1efdebb6ea71
BlenderistDev <demonx.blenderist@gmail.com>
2023-09-06 18:35:42 +0300

    add epty dir deleter

---
+++ internal/services/emptydir/delete.go
@@ -0,0 +1,58 @@
+package emptydir
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// Deleter struct for deleting empty dirs
+type Deleter struct {
+	StorageDir string
+}
+
+// DeleteEmptyDirs delete empty dirs from storage folder
+func (d Deleter) DeleteEmptyDirs() error {
+	emptyDirs, err := d.getEmptyDirs()
+	if err != nil {
+		return err
+	}
+
+	if len(emptyDirs) > 0 {
+		for _, dir := range emptyDirs {
+			err = os.Remove(dir)
+			if err != nil {
+				return err
+			}
+		}
+
+		err = d.DeleteEmptyDirs()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (d Deleter) getEmptyDirs() ([]string, error) {
+	emptyDirs := make([]string, 0)
+	if err := filepath.WalkDir(d.StorageDir, func(path string, f fs.DirEntry, err error) error {
+		if !f.IsDir() {
+			return nil
+		}
+		dir, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		if len(dir) == 0 {
+			emptyDirs = append(emptyDirs, path)
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return emptyDirs, nil
+}
---
+++ internal/services/emptydir/delete_test.go
@@ -0,0 +1,96 @@
+package emptydir
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDeleter_DeleteEmptyDirs(t *testing.T) {
+	const storageDir = "test_replacer_storage_dir"
+	defer func() {
+		_ = os.RemoveAll(storageDir)
+	}()
+
+	emptyDirPath := fmt.Sprintf("%s/%s", storageDir, "empty")
+	notEmptyDirPath := fmt.Sprintf("%s/%s", storageDir, "notEmpty")
+
+	emptyOuterDirPath := fmt.Sprintf("%s/%s", storageDir, "empty_outer")
+	emptyInnerDirPath := fmt.Sprintf("%s/%s", emptyOuterDirPath, "empty_inner")
+
+	notEmptyOuterDirPath := fmt.Sprintf("%s/%s", storageDir, "not_empty_outer")
+	notEmptyInnerDirPath := fmt.Sprintf("%s/%s", notEmptyOuterDirPath, "not_empty_inner")
+
+	err := os.MkdirAll(emptyDirPath, os.ModePerm)
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.MkdirAll(notEmptyDirPath, os.ModePerm)
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.MkdirAll(emptyInnerDirPath, os.ModePerm)
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.MkdirAll(notEmptyInnerDirPath, os.ModePerm)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = os.Create(fmt.Sprintf("%s/%s", notEmptyDirPath, "file"))
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = os.Create(fmt.Sprintf("%s/%s", notEmptyInnerDirPath, "file"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	deleter := Deleter{
+		StorageDir: storageDir,
+	}
+
+	err = deleter.DeleteEmptyDirs()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(notEmptyDirPath); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(notEmptyInnerDirPath); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(notEmptyOuterDirPath); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(emptyDirPath); !errors.Is(err, os.ErrNotExist) {
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Error("dir " + emptyDirPath + " have not been deleted")
+		}
+	}
+
+	if _, err := os.Stat(emptyOuterDirPath); !errors.Is(err, os.ErrNotExist) {
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Error("dir " + emptyOuterDirPath + " have not been deleted")
+		}
+	}
+
+	if _, err := os.Stat(emptyInnerDirPath); !errors.Is(err, os.ErrNotExist) {
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Error("dir " + emptyInnerDirPath + " have not been deleted")
+		}
+	}
+
+}
